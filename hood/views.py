74ce821d89729e792646c2e87f469d6ae347a3d4
KingVulkan <manowfelow@gmail.com>
2019-03-25 09:20:53 +0300

    creates a function for profile

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+import datetime as dt
+from django.contrib.auth.decorators import login_required
+from .models import *
 # Create your views here.
+
+def profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    hoods = Neighbourhood.objects.all()
+    return render(request, 'profile/profile.html', {"date": date, "profile":profile,"hoods":hoods})
+
+def edit_profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    if request.method == 'POST':
+        signup_form = EditForm(request.POST, request.FILES,instance=request.user.profile) 
+        if signup_form.is_valid():
+            signup_form.save()
+            return redirect('profile')
+    else:
+        signup_form =EditForm() 
+        
+    return render(request, 'profile/edit_profile.html', {"date": date, "form":signup_form,"profile":profile})
+
